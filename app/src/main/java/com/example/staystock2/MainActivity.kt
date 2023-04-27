7ba39feb88d0b41c08ffc90f12b446de9fa1d14a
gabrielaliera <gliera2990@gmail.com>
2023-04-27 15:33:40 -0700

    Update main with shi main

--- app/src/main/java/com/example/staystock2/MainActivity.kt
+++ app/src/main/java/com/example/staystock2/MainActivity.kt
@@ -2,7 +2,13 @@ package com.example.staystock2
 
 import android.os.Bundle
 import android.util.Log
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.*
@@ -13,19 +19,65 @@ import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
+
+//    below is for recyclerview
+
+    private lateinit var recyclerView: RecyclerView
+    private val productList = mutableListOf<Product>()
+
+    // below is for search
+    private lateinit var searchBar: EditText
+    private lateinit var radioGroup: RadioGroup
+    private lateinit var radioBrand: RadioButton
+    private lateinit var radioCategory: RadioButton
+
+//    below is for api call
+
     var accessToken = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // initialize the search bar and radio buttons
+        searchBar = findViewById(R.id.search_bar)
+        radioBrand = findViewById(R.id.radioBrand)
+        radioCategory = findViewById(R.id.radioCategory)
+
+        // Set up the listeners for search bar
+        searchBar.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                updateUserProductItemQuery()
+                true
+            } else {
+                false
+            }
+        }
+
+        // Set up the listeners for radio buttons
+        radioBrand.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                updateUserProductItemQuery()
+            }
+        }
+
+        radioCategory.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                updateUserProductItemQuery()
+            }
+        }
+
+        // initialize the recyclerview with the productadapter
+        recyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = ProductAdapter(productList)
+
+        // Get the authorization and update the product list for the first time
         getAuthorization()
-
-
+        updateUserProductItemQuery()
     }
 
     private fun getAuthorization() {
-
         val clientID = getString(R.string.CLIENT_ID)
         val clientSecret = getString(R.string.CLIENT_SECRET)
 
@@ -47,7 +99,7 @@ class MainActivity : AppCompatActivity() {
         client.newCall(request).enqueue(object : Callback {
             override fun onFailure(call: Call, e: IOException) {
                 // Handle error
-                Log.d("Autho Error", "Error" )
+                Log.d("Autho Error", "Error")
             }
 
             override fun onResponse(call: Call, response: Response) {
@@ -57,23 +109,52 @@ class MainActivity : AppCompatActivity() {
                     val responseBody = response.body?.string()
                     accessToken = JSONObject(responseBody).getString("access_token")
                     Log.d("Autho Token", "$accessToken")
-                    //getFoodCategoryInfo()
-                    getFoodBrandInfo()
 
+                    // Call updateUserProductItemQuery() here
+                    runOnUiThread {
+                        updateUserProductItemQuery()
+                    }
                 } else {
                     Log.d("Autho Response Error", "error")
                 }
-
             }
         })
+    }
 
+    private fun getFoodInfoAsy(){
+        val  userProductItemQuery = "food"
+        val foodURL = "https://api.kroger.com/v1/products?filter.term=$userProductItemQuery&filter.item=100"
+        val client = AsyncHttpClient()
+
+        client[foodURL, object : JsonHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Headers,
+                json: JsonHttpResponseHandler.JSON
+            ) {
+                Log.d("Autho Food JSON", "query response successful $json")
+
+                val dataObject = json.jsonObject.getJSONObject("data")
+
+
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                errorResponse: String,
+                throwable: Throwable?
+            ) {
+                Log.d("Autho Food Error", errorResponse)
+            }
+        }]
 
     }
 
 
-    private fun getFoodCategoryInfo(){
-        val  userProductItemQuery = "milk"
-        val foodURL = "https://api.kroger.com/v1/products?filter.term=$userProductItemQuery&filter.item=10"
+    private fun getFoodInfo(searchTerm: String, filterBy: String){
+//        val  userProductItemQuery = "food"
+        val foodURL = "https://api.kroger.com/v1/products?filter.term=$searchTerm&filter.$filterBy=30"
         val client = OkHttpClient()
 
         val request = Request.Builder()
@@ -98,108 +179,45 @@ class MainActivity : AppCompatActivity() {
                     Log.d("Autho Food Json", "$jsonObject")
 
                     //Get Data
+                    //Update the onResponse method in getFoodInfo()
+                    // to populate the productList with the data from the API and update the RecyclerView:
                     val data = jsonObject.getJSONArray("data")
-                    Log.d("Autho Food data", "$data")
 
-                    //Get info from first item out of 10
-                    val firstItem = data.getJSONObject(3);
-                    Log.d("Autho Food First Item", "$firstItem")
+                    for (i in 0 until data.length()) {
+                        val item = data.getJSONObject(i)
+                        val productId = item.optString("productId")
+                        val productName = item.optString("description")
+                        Log.d("ProductName", "Product Name: $productName")
+                        val brandName = item.optString("brand")
+                        val productSize = item.getJSONArray("items").getJSONObject(0).optString("size") // Use first item for size
+                        val categoryId = item.optString("countryOrigin")
+                        val category = categoryId // Replace this with an actual category lookup based on categoryId
 
-                    //Get productName
-                    val productName = firstItem.optString("description").toString()
-                    Log.d("Autho Food Description", "$productName")
+                        val imageJSONObject = item.getJSONArray("images").getJSONObject(0)
+                        val imageSizes = imageJSONObject.getJSONArray("sizes")
+                        val imageInfo = imageSizes.getJSONObject(1)
+                        val imageUrl = imageInfo.optString("url")
 
-                    //Get BrandName
-                    val brandName = firstItem.optString("brand").toString()
-                    Log.d("Autho Food Brand", "$brandName")
+                        productList.add(Product(productId, productName, brandName, category, productSize, imageUrl))
+                    }
 
-                    //Price is missing in jsonobject
-                    val price = firstItem.getJSONArray("items")
-                    Log.d("Autho Price", "$price")
-
-                    //image
-                    val imageJSONObject = firstItem.getJSONArray("images").getJSONObject(0)
-                    val imageSizes = imageJSONObject.getJSONArray("sizes")
-                    val imageInfo = imageSizes.getJSONObject(1)
-                    val imgURL = imageInfo.optString("url")
-                    Log.d("Autho Image JSON Object", "$imageJSONObject")
-                    Log.d("Autho Image Sizes", "$imageSizes")
-                    Log.d("Autho Image Thumbnail Info", "$imageInfo")
-                    Log.d("Autho Image url", "$imgURL")
-
-                } else {
-                    // Handle error
-                    Log.d("Autho Food Response Error", "error")
+                    runOnUiThread {
+                        recyclerView.adapter?.notifyDataSetChanged()
+                    }
                 }
             }
         })
     }
 
-    private fun getFoodBrandInfo(){
-        val  userProductItemQuery = "bread"
-        val  userBrand = "Kroger"
-        val fulfilmentStatus = "csp"
-        val foodURL = "https://api.kroger.com/v1/products?filter.term=$userProductItemQuery&filter.brand=$userBrand&filter.fulfillment=$fulfilmentStatus&filter.limit=10"
-        val client = OkHttpClient()
+    private fun updateUserProductItemQuery() {
+        var searchTerm = searchBar.text.toString()
 
-        val request = Request.Builder()
-            .url(foodURL)
-            .addHeader("Accept", "application/json")
-            .addHeader("Authorization", "Bearer $accessToken")
-            .get()
-            .build()
+        // Use "food" as the default searchTerm if the search bar is empty
+        if (searchTerm.isEmpty()) {
+            searchTerm = "food"
+        }
 
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                // Handle failure
-                Log.d("Autho FAILURE", "failure")
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                if (response.isSuccessful) {
-                    val responseBody = response.body?.string()
-                    // Parse the response to JSON Object
-                    Log.d("Autho Brand Response success", "$responseBody")
-                    val jsonObject = JSONObject(responseBody)
-                    Log.d("Autho Food Json", "$jsonObject")
-
-                    //Get Data
-                    val data = jsonObject.getJSONArray("data")
-                    Log.d("Autho Food data", "$data")
-
-                    //Get info from first item out of 10
-                    val firstItem = data.getJSONObject(5);
-                    Log.d("Autho Food First Item", "$firstItem")
-
-                    //Get productName
-                    val productName = firstItem.optString("description").toString()
-                    Log.d("Autho Food Description", "$productName")
-
-                    //Get BrandName
-                    val brandName = firstItem.optString("brand").toString()
-                    Log.d("Autho Food Brand", "$brandName")
-
-                    //Price is missing in jsonobject
-                    val price = firstItem.getJSONArray("items")
-                    Log.d("Autho Price", "$price")
-
-                    //image
-                    val imageJSONObject = firstItem.getJSONArray("images").getJSONObject(0)
-                    val imageSizes = imageJSONObject.getJSONArray("sizes")
-                    val imageInfo = imageSizes.getJSONObject(0)
-                    val imgURL = imageInfo.optString("url")
-                    Log.d("Autho Image JSON Object", "$imageJSONObject")
-                    Log.d("Autho Image Sizes", "$imageSizes")
-                    Log.d("Autho Image Thumbnail Info", "$imageInfo")
-                    Log.d("Autho Image url", "$imgURL")
-
-                } else {
-                    // Handle error
-                    Log.d("Autho B Response Error", "$response")
-                }
-            }
-        })
+        val filterBy = if (radioBrand.isChecked) "brand" else "category"
+        getFoodInfo(searchTerm, filterBy)
     }
-
-
 }
